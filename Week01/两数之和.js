cf5c15d1d0a5331c870dc88e7ca3cdc0ba2dc6f1
Jiangchunhui <17853538658@163.com>
2020-06-14 16:26:26 +0800

    第一周的作业 两数字和 删除排序数组中的重复项 还有学习终结 其他的题目前不看题解 做不出来 就没提交

---
+++ Week01/两数之和.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+// 两数之和
+var twoSum = function(nums, target) {
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i; j < nums.length; j++){
+            if(nums[i]+nums[j] == target&&i != j){
+                return [i,j] 
+            }
+        }
+    }
+};
